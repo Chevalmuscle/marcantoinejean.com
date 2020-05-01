40361967c75b64772ec83addd6b5df02c8f03159
chevalmuscle <me@marcantoinejean.com>
2020-05-01 02:57:45 -0400

    purged placeholder homepage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import React from "react";
-
-import solarSystemImage from "./assets/images/solar-system.png";
+import React, { Component } from "react";
 import "./App.css";
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
-    return (
-      <div>
-        <div class="text-container">
-          <h1 className="text high-emphasis">Marc-Antoine Jean</h1>
-          <div>
-            <div className="text high-emphasis">
-              Work in progress{" "}
-              <span role="img" aria-label="smiley">
-                🙂
-              </span>
-            </div>
-            <div className="text high-emphasis">
-              Contact me via{" "}
-              <a href="mailto:me@marcantoinejean.com" target="_blank">me@marcantoinejean.com</a>
-            </div>
-          </div>
-        </div>
-        <img id="solar-system" src={solarSystemImage} alt="solar-system" />
-      </div>
-    );
+    return <div>Hello</div>;
   }
 }
