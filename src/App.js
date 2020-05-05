a5f6db94f1a85378e87a188428296e4a728c10e2
chevalmuscle <me@marcantoinejean.com>
2020-05-05 03:17:52 -0400

    done first section (about)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
-import "./App.css";
+
+import styles from "./App.module.css";
 
 export default class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <div className={`${styles["section"]} ${styles["dark"]}`}>
+          <div className={`${styles["first-container"]}`}>
+            <h1 className={`${styles["high-emphasis"]} ${styles["big-title"]}`}>Marc-Antoine Jean</h1>
+            <div className={`${styles["high-emphasis"]} ${styles["sub-title"]}`}>
+              Bonjour ! Je suis un étudiant en génie logiciel à l’École de technologie supérieure à Montréal et prévois
+              graduer en décembre 2020.
+              <br />
+              Il me reste un stage de 4 mois à effectuer.
+            </div>
+            <button className={styles["see-projet-button"]}>Voir mes projets</button>
+          </div>
+        </div>
+        <div className={`${styles["section"]} ${styles["medium"]}`}></div>
+        <div className={`${styles["section"]} ${styles["light"]}`}></div>
+        <div className={`${styles["section"]} ${styles["medium"]}`}></div>
+        <div className={`${styles["section"]} ${styles["light"]}`}></div>
+      </div>
+    );
   }
 }
