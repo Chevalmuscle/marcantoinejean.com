b50b8952fea797dea02743de921400dccbf347bf
chevalmuscle <me@marcantoinejean.com>
2020-05-08 17:50:06 -0400

    added mail sending for the contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
-import "./ContactForm.module.css";
+import Spinner from "./Spinner";
+import { MAILING_SERVER_URL } from "../config";
+
+import styles from "./ContactForm.module.css";
 
 export default class ContactForm extends Component {
   constructor(props) {
@@ -9,10 +15,25 @@ export default class ContactForm extends Component {
       name: "",
       email: "",
       message: "",
+      fetchInProgress: false,
     };
 
     this.handleOnChange = this.handleOnChange.bind(this);
     this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    toast.configure({
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      newestOnTop: false,
+      closeOnClick: true,
+      rtl: false,
+      pauseOnFocusLoss: true,
+      draggable: false,
+      pauseOnHover: true,
+    });
   }
 
   handleOnChange(stateProp, value) {
@@ -21,19 +42,64 @@ export default class ContactForm extends Component {
 
   handleOnSubmit(e) {
     e.preventDefault();
-    console.log(this.state);
+
+    // prevent spam click
+    if (this.state.fetchInProgress) return;
+
+    this.setState({ fetchInProgress: true });
+    axios
+      .post(`${MAILING_SERVER_URL}/mail`, {
+        name: this.state.name,
+        email: this.state.email,
+        message: this.state.message,
+      })
+      .then((response) => {
+        toast.success("Email envoyé 🚀");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Erreur dans l'envoie du email 🥺");
+      })
+      .finally(() => {
+        this.setState({ fetchInProgress: false });
+      });
   }
 
   render() {
     return (
       <form onSubmit={this.handleOnSubmit}>
-        <label>Nom</label>
-        <input type="text" name="name" onChange={(e) => this.handleOnChange("name", e.target.value)} />
-        <label>Email</label>
-        <input type="text" name="email" onChange={(e) => this.handleOnChange("message", e.target.value)} />
-        <label>Message</label>
-        <textarea type="text" name="message" onChange={(e) => this.handleOnChange("email", e.target.value)} />
-        <input type="submit" value="Envoyer" />
+        <label htmlFor="name">Nom</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          onChange={(e) => this.handleOnChange("name", e.target.value)}
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          onChange={(e) => this.handleOnChange("email", e.target.value)}
+          required
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          type="text"
+          name="message"
+          onChange={(e) => this.handleOnChange("message", e.target.value)}
+          required
+        />
+        <div className={styles["sent-button-container"]}>
+          <input type="submit" value="Envoyer" />
+          {this.state.fetchInProgress && (
+            <div className={styles["is-fetching-spinner"]}>
+              <Spinner />
+            </div>
+          )}
+        </div>
       </form>
     );
   }
