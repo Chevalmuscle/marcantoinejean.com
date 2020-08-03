2bd4299d62cce6749d487263fa1b0a4f101dd00f
chevalmuscle <me@marcantoinejean.com>
2020-08-02 23:29:18 -0400

    🧼 replaced css modules by styled components

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,22 +1,40 @@
 import React from "react";
 import { useI18n } from "react-simple-i18n";
-import styles from "./Resume.module.css";
+import styled from "styled-components";
 import ResumePreview from "../assets/svg/resume.svg";
 import FileDownloadIcon from "../assets/svg/file-download-outline.svg";
+
+const ResumeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const DownloadFileContainer = styled.a`
+  display: flex;
+`;
+
+const ResumePreviewContainer = styled.div`
+  border: 1px solid black;
+  border-radius: 2px;
+  @media only screen and (max-width: 600px) {
+    display: none;
+  }
+`;
 
 function Resume() {
   const { t } = useI18n();
 
   return (
-    <div className={styles["resume-section"]}>
-      <a className={styles["download-file-container"]} href="/MarcAntoineJean_Resume.pdf" download>
+    <ResumeContainer>
+      <DownloadFileContainer href="/MarcAntoineJean_Resume.pdf" download>
         <img src={FileDownloadIcon} alt=" " />
         {t("resume.download")}
-      </a>
-      <div className={styles["resume-preview"]}>
+      </DownloadFileContainer>
+      <ResumePreviewContainer>
         <img src={ResumePreview} alt="resume preview" />
-      </div>
-    </div>
+      </ResumePreviewContainer>
+    </ResumeContainer>
   );
 }
 
